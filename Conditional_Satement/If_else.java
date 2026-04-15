c1e98253c9cd006cd6342561898c2c87e7812195
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-04-15 23:52:00 +0530

    corejava

---
+++ Conditional_Satement/If_else.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class If_else{
+    public static void main(String [] args){
+
+       int age=15;
+
+       if(age>=18){
+         System.out.println("Able to Drive the car");
+       }
+       if(age>13&& age<18){
+        System.out.println("this is teenager");
+       }
+       else{
+        System.out.println("You are not abel to drive");
+       }
+
+    }
+}
